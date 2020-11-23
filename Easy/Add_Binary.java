c2eaa27fa980ee965fdf4f234e7e576e4ffe0eae
Nick Perinovic <48705263+nperinovic@users.noreply.github.com>
2020-11-22 18:48:54 -0600

    Update Add_Binary.java

--- Easy/Add_Binary.java
+++ Easy/Add_Binary.java
@@ -28,8 +28,8 @@ class Solution {
                 sum += b.charAt(bTemp--) - '0'; // - '0' will give us 0 if a.charAt(aTemp--) is '0' and 1 if a.charAt(aTemp--) = '1'
             }
             
-            str.insert(0, sum % 2);
-            carry = sum / 2;
+            str.insert(0, sum % 2); // inserting at 0 will shift everything back one. sum % 2 since sum is either 0, 1, or 2
+            carry = sum / 2; // If sum is every 2 we will get a 1, meaning that we need to shift. If it's a 1 or 2 this will give a carry of 0
         }
         
         if(carry > 0) {
