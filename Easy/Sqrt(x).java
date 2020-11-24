1c742ab970149b01adfff59e13b21b4b84611ebb
Nick Perinovic <48705263+nperinovic@users.noreply.github.com>
2020-11-24 13:25:24 -0600

    Update Sqrt(x).java

--- Easy/Sqrt(x).java
+++ Easy/Sqrt(x).java
@@ -19,3 +19,22 @@ class Solution {
         return (int)Math.sqrt((double)x);
     }
 }
+
+//or 
+
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1, right = x;
+        if (x==0 || x==1) return x;
+        
+        while(true){
+            int mid = left + (right-left)/2;
+            if (mid <=  x/mid && mid+1 > x/(mid+1))
+                return mid; 
+            if(mid < x/mid)
+                left = mid + 1;
+            else 
+                right = mid-1;
+        }
+    }
+}
