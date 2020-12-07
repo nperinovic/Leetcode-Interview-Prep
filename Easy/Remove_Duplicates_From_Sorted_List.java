15ea0752dfc86625077fe9c322978fe655742e45
Nick Perinovic <nperin17@gmail.com>
2020-12-07 16:36:30 -0600

    duplicate problem

---
+++ Easy/Remove_Duplicates_From_Sorted_List.java
@@ -0,0 +1,25 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while(temp != null && temp.next != null) {
+            if(temp.next.val == temp.val) {
+                temp.next = temp.next.next;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
